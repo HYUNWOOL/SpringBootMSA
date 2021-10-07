f6f4f6c231501d6543755dcf0664e634958adcfe
hyunwoo <kiesland1121@naver.com>
2021-10-07 14:58:11 +0900

    20211007 loadbalancer in springcloud

--- first-service/src/main/java/com/example/zuul/FirstServiceController.java
+++ first-service/src/main/java/com/example/zuul/FirstServiceController.java
@@ -1,13 +1,25 @@
 package com.example.zuul;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/first-service")
 public class FirstServiceController {
+
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to the first Service";
@@ -21,8 +33,9 @@ public class FirstServiceController {
 
 
     @GetMapping("/check")
-    public String check(){
-        return "hi in to the first Service";
+    public String check(HttpServletRequest request){
+    System.out.println("server port : " + request.getServerPort());
+        return String.format("hi in to the first Service on port %s",env.getProperty("local.server.port"));
     }
 
 
